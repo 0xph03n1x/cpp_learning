2ef2d5bdd3ba05ed7438f46669ec0133b0b07a13
0xph03n1x <mlazarov@Mihails-MacBook-Air.local>
2022-01-27 15:42:53 +0200

    initial commit

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+//Hello World
+using namespace std;
+
+//This is where the execution of program begins
+int main()
+{
+   // displays Hello World! on screen
+   cout<<"Hello World!"<<endl;
+
+   return 0;
+} 
