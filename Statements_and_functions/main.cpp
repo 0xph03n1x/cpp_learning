861142ed261591b008e3951977067b5f58836094
0xph03n1x <lazarovmihail97@gmail.com>
2022-03-17 14:20:30 +0200

    Statements and fucntions

--- Statements_and_functions/main.cpp
+++ Statements_and_functions/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+int addNumbers(int first_param, int second_param){
+
+    int result = first_param + second_param;
+    return result;
+}
+
 int main() {
-    auto result = (10 <=> 20) > 0;
-    std::cout << result << std::endl;
+
+    // int FirstNumber = 12;
+    // int SecondNumber = 9;
+
+    // int sum = FirstNumber + SecondNumber;
+    // std::cout << "The sum of the two numbers is: " << sum << std::endl;
+
+    int FirstNumber {13};    //Statement
+    int SecondNumber {7};
+    
+
+    std::cout << "First number: " << FirstNumber << std::endl;  //13
+    std::cout << "Second number: " << SecondNumber << std::endl;    //7
+
+    int sum = FirstNumber + SecondNumber;
+    std::cout << "Sum: " << sum << std::endl;   //20
+    
+    sum = addNumbers(25,7);
+    std::cout << "Sum: " << sum << std::endl;   //32
+
+    sum = addNumbers(30,55);
+    std::cout << "Sum: " << sum << std::endl;   //85
+
+    sum = addNumbers(-1, 5);
+    std::cout << "Sum: " << sum << std::endl;   //4
+
+    std::cout << "Sum: " << addNumbers(4,55) << std::endl;   //59
+
+    return 0;
+
 }
