c56dc4e290b5bcf94b2ee41d571c401006791ba1
0xph03n1x <lazarovmihail97@gmail.com>
2022-06-18 02:57:26 +0300

    update

---
+++ CodeCademy/leap_year.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+  int year = 0;  
+  std::cout << "Type a year: ";
+  std::cin >> year;
+
+  if (year < 1000 || year > 9999){
+    std::cout << "Try again.";
+  }
+  else if ( year % 4 == 0 && year % 100 !=0 || year % 400 == 0) {
+    std::cout << year;
+    std::cout << " is a leap year.\n";
+  }
+  else {
+    std::cout << year;
+    std::cout << " is not a leap year.\n";
+  }
+  
+}
